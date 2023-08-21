25f268c1d71190a91b73a35d0650ffb035487496
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-08-21 08:19:12 +0100

    added functionalities

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const input = document.getElementById("input");
+const infoText = document.getElementById("info-text");
+const meaningContainer = document.getElementById("meaning-container");
 
 async function fetchAPI(word){
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-    const result = await fetch(url).then((res)=>res.json())
-    console.log(result);
+    // allows you to get the possible error from an API request
+    try {
+        infoText.style.display = "block";
+        infoText.innerText = `Searching for the meaning of ${word}`
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        const result = await fetch(url).then((res)=>res.json())
+        console.log(result);
+        infoText.style.display = "none";
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 input.addEventListener("keyup", (e)=> {
